9adf6499c777ec2f48b931944461f38c4fbcb5b3
viktorfrom <viktor.from91@gmail.com>
2019-02-13 10:33:54 +0100

    "Update remote, install, embedded"

--- embedded.sh
+++ embedded.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-#IP address of LiDAR, needs to be configured manually
-IPADDR = '127.168.0.1'
-bash --rcfile <(echo '. ~/.bashrc; roslaunch lms1xx LMS1xx.launch host:=$IPADDR')
-bash --rcfile <(echo '. ~/.bashrc; rosrun lms1xx LMS1xx_node')
-bash --rcfile <(echo '. ~/.bashrc; rosrun embedded talker.py')
+#NOTICE: IP address of LiDAR, needs to be configured manually
+#bash --rcfile <(echo '. ~/.bashrc; roslaunch lms1xx LMS1xx.launch host:=169.254.152.4')
+#bash --rcfile <(echo '. ~/.bashrc; rosrun lms1xx LMS1xx_node')
+#bash --rcfile <(echo '. ~/.bashrc; rosrun embedded talker.py')
+
+
+function teardown {
+  killall -9 embedded.sh
+}
+
+trap teardown EXIT
+
+# Start script 1
+nohup roslaunch lms1xx LMS1xx.launch host:=169.254.152.5 > ~/catkin_ws/src/remote/scripts/LMS1xx.launch 2>&1 
+
+# Start script 2
+nohup rosrun lms1xx LMS1xx_node > ~/catkin_ws/src/remote/scripts/LMS1xx_node 2>&1
+
+rosrun embedded talker.py
--- remote.sh
+++ remote.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-bash --rcfile <(echo '. ~/.bashrc; rosrun remote HMI.py')
-bash --rcfile <(echo '. ~/.bashrc; rosrun remote listener.py')
+#bash --rcfile <(echo '. ~/.bashrc; roscore')
+#bash --rcfile <(echo '. ~/.bashrc; rosrun remote HMI.py')
+#bash --rcfile <(echo '. ~/.bashrc; rosrun remote listener.py')
+
+function teardown {
+  killall -9 remote.sh
+}
+
+trap teardown EXIT
+
+# Start script 1
+nohup roscore > ~/catkin_ws/src/remote/scripts/roscore 2>&1 
+
+# Start script 2
+nohup rosrun remote listener.py > ~/catkin_ws/src/remote/scripts/listener.py 2>&1 
+
+rosrun remote HMI.py
